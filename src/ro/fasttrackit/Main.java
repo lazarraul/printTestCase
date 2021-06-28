693510fa25ef93ef631b347120fbbd948a07dc78
lazarraul <raul_lazar07@yahoo.com>
2021-06-28 12:51:38 +0300

    first commit

---
+++ src/ro/fasttrackit/Main.java
@@ -0,0 +1,12 @@
+package ro.fasttrackit;
+
+import java.util.Arrays;
+
+public class Main {
+
+    public static void main(String[] args) {
+    //      String nameToPrint =args[0];
+    //    String secondNameToPrint = args[1];
+        System.out.println("Hello World! "+ Arrays.toString(args));
+    }
+}
