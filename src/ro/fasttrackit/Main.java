87b5a4c44f71c03d7cbff4b8da074c91dedb8e0b
lazarraul <raul_lazar07@yahoo.com>
2021-06-29 11:02:26 +0300

    TESTCASE

--- src/ro/fasttrackit/Main.java
+++ src/ro/fasttrackit/Main.java
@@ -1,12 +1,81 @@
 package ro.fasttrackit;
+import java.util.ArrayList;
 
-import java.util.Arrays;
 
 public class Main {
 
     public static void main(String[] args) {
-    //      String nameToPrint =args[0];
-    //    String secondNameToPrint = args[1];
-        System.out.println("Hello World! "+ Arrays.toString(args));
+
+       ArrayList<String> title = new ArrayList<String>();
+
+        title.add("\t\tId");
+        title.add("\t\t\t\t\tActionItem ");
+        title.add("\t\t\t\t\t\t\t\t\t\t\tExpectedResults ");
+        title.add("\t\t\t\t\t\t\t\t\t\t\tPass/Fail");
+        System.out.println(title);
+        //Title from my Test Case
+        System.out.println("\tTC_DS_01");
+        System.out.println("Pre-req:Username:dino,beetle,turtle,Password:choochoo");
+        System.out.println();
+
+        ArrayList<String> id = new ArrayList<String>();
+        id.add("\t\t1");
+        id.add("\t\t2");
+        id.add("\t\t3");
+        id.add("\t\t4");
+        {
+            int i = 0;
+            while (i < id.size()) {
+                i++;
+            }
+        }
+        //adding number id
+        ArrayList<String> aItem = new ArrayList<String>();
+        aItem.add("\t\t\t\t\t\tOpen Home Page");
+        aItem.add("\t\t\t\t\t\tClick on Login button in the right corner");
+        aItem.add("\t\t\t\t\t\tType the username and password");
+        aItem.add("\t\t\t\t\t\tClick on Login button ");
+
+        {
+            int i = 0;
+            while (i < aItem.size()) {
+                i++;
+            }
+        }
+
+        //adding Action Item
+
+        ArrayList<String> eResults = new ArrayList<String>();
+        eResults.add("\t\t\t\t\t\t\t\t\t\t\tHome Page is open");
+        eResults.add("\t\t\t\tForm with username and password is open");
+        eResults.add("\t\t\t\t\t\t\tForm is complete");
+        eResults.add("\t\t\t\t\t\t\t\t\tUser is logged in");
+
+        {
+            int i = 0;
+            while (i < eResults.size()) {
+                i++;
+            }
+        }
+
+        //adding Expected Results
+
+        ArrayList<String> pf = new ArrayList<String>();
+        pf.add("\t\t\t\t\t\t\t\t\t\t\tPASS");
+        pf.add("\t\t\t\t\t\tPASS");
+        pf.add("\t\t\t\t\t\t\t\t\t\t\tPASS");
+        pf.add("\t\t\t\t\t\t\t\t\t\t\tPASS");
+
+        for (int i = 0; i < aItem.size(); i++) {
+            System.out.println(id.get(i) + aItem.get(i) + eResults.get(i) + pf.get(i)); }
+        //Pass OR Fail
+        //end of test
+
+
+
+
+
+
+
     }
 }
---
+++ src/ro/fasttrackit/creatingtablefortastcase.java
@@ -0,0 +1,5 @@
+package ro.fasttrackit;
+import java.sql.*;
+
+public class creatingtablefortastcase {
+}
